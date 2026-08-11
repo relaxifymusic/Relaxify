daa03f622736d2aa9bfccf53c4c75fbab0a23db4
relaxifymusic <djboy4696@gmail.com>
2026-08-12 00:05:23 +0530

    Add configuration for YouTube Data API

---
+++ config.js
@@ -0,0 +1,15 @@
+window.RELAXIFY_CONFIG = {
+
+  // YouTube Data API key yahan paste karna hai
+  YOUTUBE_API_KEY: "",
+
+  // India
+  REGION_CODE: "IN",
+
+  // Hindi/relevant search results
+  RELEVANCE_LANGUAGE: "hi",
+
+  // Search typing ke baad kitni jaldi search ho
+  SEARCH_DEBOUNCE_MS: 180
+
+};
