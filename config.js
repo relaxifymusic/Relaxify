7a62a87c3461b2ca05f72a594f58e4e32bff6b4c
relaxifymusic <djboy4696@gmail.com>
2026-08-12 17:15:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,6 @@
 window.RELAXIFY_CONFIG = {
-
-  // YouTube Data API key yahan paste karna hai
-  YOUTUBE_API_KEY: "",
-
-  // India
-  REGION_CODE: "IN",
-
-  // Hindi/relevant search results
-  RELEVANCE_LANGUAGE: "hi",
-
-  // Search typing ke baad kitni jaldi search ho
-  SEARCH_DEBOUNCE_MS: 180
-
+YOUTUBE_API_KEY: "AIzaSyDEAUGeZE1jKe2XesFUScwhwZHJhlyXoF0",
+REGION_CODE: "IN",
+RELEVANCE_LANGUAGE: "hi",
+SEARCH_DEBOUNCE_MS: 180
 };
